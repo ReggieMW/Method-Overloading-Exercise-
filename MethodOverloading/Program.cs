88cbc9b4a11a1435f89a302d9014dc27603d54a5
ReggieMW <reggiewoods@msn.com>
2025-03-07 15:04:01 -0600

    updated to call all three methods

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -7,9 +7,9 @@
             return a + b;
         }
         
-        public static decimal Add(decimal a, decimal b)
+        public static decimal Add(decimal c, decimal d)
         {
-            return a + b;
+            return c + d;
         }
 
         public static string Add(int a, int b, bool one)
@@ -35,9 +35,29 @@
         }
         static void Main(string[] args)
         {
-            var a = -11;
-            var b = 76;
+            int a = -1;
+            int b = 2;
+            decimal c = 2.0m;
+            decimal d = -1.0m;
             bool one = true;
+            string oneDollar = " dollar";
+            string twoDollar = " dollars";
+            
+            if (Add(a, b) == 1)
+                Console.WriteLine(a + b + oneDollar);
+            else
+            {
+                Console.WriteLine(a + b + twoDollar);
+            }
+
+            if (Add(c, d) == 1)
+            {
+                Console.WriteLine(c + d + oneDollar);
+            }
+            else
+            {
+                Console.WriteLine(c + d + twoDollar);
+            }
             
             Console.WriteLine(Add(a, b, one));
         }
