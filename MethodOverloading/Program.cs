cba794e7c607811edd4347363fc44aaf1826a468
ReggieMW <reggiewoods@msn.com>
2025-03-07 11:06:54 -0600

    method created and overloaded *2, code entered to recognize 1 dollar.

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,44 @@
 {
     public class Program
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool one)
+        {
+            var sum = a + b;
+            
+            if (one == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (one == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (one == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = -11;
+            var b = 76;
+            bool one = true;
+            
+            Console.WriteLine(Add(a, b, one));
         }
     }
 }
